3fe079403b4102d9a1aa4c49160375429cf0514c
fangdongdong <fd7637@163.com>
2018-04-11 10:50:23 +0800

    URL添加

--- mainapp/mainurl.py
+++ mainapp/mainurl.py
@@ -6,4 +6,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^index/$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^help/$', views.help, name='help'),
     ]
