338234be6969aea9397652a526493df25b3dd06e
fangdongdong <fd7637@163.com>
2018-04-11 10:32:02 +0800

    页面显示

--- mainapp/adminx.py
+++ mainapp/adminx.py
@@ -4,6 +4,13 @@
 """
 import xadmin
 from xadmin import views
+from .models import Area
+
+
+class Area1(object):
+    list_display = ['id', 'name', 'parent_area', 'community', 'level']
+    list_filter = ['name']
+    search_filter = ['name']
 
 
 class GlobalSetting(object):
@@ -11,4 +18,5 @@ class GlobalSetting(object):
     site_footer = 'PS社区'
 
 
+xadmin.site.register(Area, Area1)
 xadmin.site.register(views.CommAdminView, GlobalSetting)
