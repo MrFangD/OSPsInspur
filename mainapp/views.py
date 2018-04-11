02fd1b293d2c7de7aca3cb6a7a9da0d0106d1416
fangdongdong <fd7637@163.com>
2018-04-11 10:55:54 +0800

    import

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,22 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from . import models
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render_to_response
 
 
 def index(request):
     return render(request, "index.html",)
+
+
+def login(request):
+    nodes = models.Area.objects.all()
+    return render(request, "login.html", {'nodes': nodes})
+
+
+def register(request):
+    return render(request, "register.html")
+
+
+def help(request):
+    nodes = models.Area.objects.all()
+    return render(request, 'help.html', {'nodes': nodes})
